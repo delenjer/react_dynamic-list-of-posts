d7f616cff0a016e302ee5e23af740d7f9c10394c
Stanislav Mochyr <delenjer@gmail.com>
2020-03-09 13:26:30 +0200

    Updates

---
+++ precache-manifest.4766a725045a23d831cbc6f95196213b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c7dfc793728db73b0c3b319495b88a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "044108415f5a75ca916b",
+    "url": "./static/css/main.5d09efd7.chunk.css"
+  },
+  {
+    "revision": "b6e7b491a681892c5e39",
+    "url": "./static/js/2.fa5b4558.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.fa5b4558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044108415f5a75ca916b",
+    "url": "./static/js/main.7d1c1d81.chunk.js"
+  },
+  {
+    "revision": "acb15a54c7276c556e90",
+    "url": "./static/js/runtime-main.8f6a98c8.js"
+  }
+]);
